f724505bf97947cee4b2153310c87af43c070396
Naxocist <73634957+Naxocist@users.noreply.github.com>
2021-09-01 15:46:23 +0700

    Rebuild .anime function
    change to embeded message

--- main.py
+++ main.py
@@ -1,53 +1,26 @@
-import sqlite3
 import pytz
 import discord
 import os
 import asyncio
+from math import *
 from random import *
 from alive import alive
 from datetime import *
-
-def add(i):
-  conn = sqlite3.connect('anime.db')
-  c = conn.cursor()
-  c.execute(f"INSERT INTO animes VALUES ('{i}')")
-  print(f"Added {i}")
-  conn.commit()
-  conn.close()
-
-
-def remove(id):
-  conn = sqlite3.connect('anime.db')
-  c = conn.cursor()
-  c.execute(f"DELETE from animes WHERE rowid={id}")
-  conn.commit()
-  conn.close()
-
-def show_list():
-  conn = sqlite3.connect('anime.db')
-  c = conn.cursor()
-  c.execute("SELECT rowid, * FROM animes")
-
-  r=[i[1] for i in c.fetchall()]
-  print(r)
-  return r
-
+from animelist import * 
 
 command = {
   '.date':'Show current date.',
   '.anime':'Random an anime for u. :)',
-  '.addanime':'Add an anime in random list.',
-  '.list':'Show all animes in database.'
   }
 bot = discord.Client()
 
+
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for date"))
+    print("Chillin' is ready for his duty.")
 
 @bot.event
 async def on_message(message):
     send=message.channel.send
-
     msg = message.content.lower()
     if message.author == bot.user:
         return
@@ -66,40 +39,32 @@ async def on_message(message):
         now = datetime.now(pytz.timezone('Asia/Bangkok'))
         await send(now.strftime("`📅 %A, %d %b %Y %H:%M%p `"))
     
-    if msg.startswith(".anime"): # Random Anime
-        e = ["╰(▔∀▔)╯", "( Φ ω Φ )"]
-        a1 = choice(e);e.pop(e.index(a1));a2 = choice(e)
-        title = f"`{a1}  Random Anime  {a2}`"
-        l = len(title)
-        if l%2==0:c = len(title)/2
-        else:c = len(title)/2+0.5
-        ans = "‎‎ "*int(c)
-
-        async with message.channel.typing():
-          await asyncio.sleep(0.5)
-
-        await send(f"{title}\n")
+    if message.content.startswith(".anime"): # Random Anime
+        i = list(animes.items())
+        anime = choice(i)[0]
+        e = ["(o´▽`o)", "(¯▿¯)", "(˙꒳​˙)"]
+        a1 = choice(e);e.pop(e.index(a1));a2 = choice(e) 
         
-        await send(ans + f"⥅  `{choice(show_list()).capitalize()}`  ⥆")
+        embed = discord.Embed(title=f"{anime}", 
+        url=f"{link[anime]}" ,
+        description="ไปดูเมะกันดีกว่า", 
+        colour = discord.Colour.random())
 
-    if msg.startswith(".addanime"): # Add Anime in List
-        i=msg.replace(".addanime", "")
-        i=i.strip()
-        add(i);print(i)
-        await send(f"Added {i}!")
+        embed.set_author(name=message.author.display_name, 
+        icon_url=message.author.avatar_url)
 
+        g = genre[anime].replace("[", " ").replace("]", " ").replace("\'", " ")
+        embed.add_field(name="ประเภท", value=g, inline=False)
+        
+        if ep[anime] == "1": episode="เป็นเดอะมูฟวี่"
+        else: episode=f"มี {ep[anime]} ตอน"
+        embed.add_field(name=episode, value="="*len(episode), inline=True)
+        embed.set_image(url=f"{pic[anime]}")
+        r = rank[anime]
+        if r == "":r="ไม่จัดอันดับ"
+        embed.add_field(name=f"อันดับอนิเมะ: {r}", value="="*len(episode), inline=True)
+        embed.set_footer(text="Bot developed  by `Naxocist`")     
+        await send(embed=embed)
 
-    if msg.startswith(".list"):
-        db = show_list()
-        async with message.channel.typing():
-          await asyncio.sleep(0.25)
-        await send(f"`{db}`")
-
-    if msg.startswith(".remove"):
-        i=msg.replace(".remove", "")
-        remove(i)
-        await send("Remove Successfully!")
-
-    # Index function
 alive()
 bot.run('ODc3NDI1Mzg0ODY0NTAxNzYw.YRycEQ.qLrJI_seoWMFxZ6dk3O83pCY568')
