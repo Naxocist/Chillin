406770e8a7fc681d9bf611b21e09db1e4a7c4282
Naxocist <73634957+Naxocist@users.noreply.github.com>
2021-09-03 11:25:32 +0700

    Using discord.ext lib

--- main.py
+++ main.py
@@ -1,73 +1,66 @@
-import pytz
-import discord
-import asyncio
+from discord.ext import commands
 from random import *
 from alive import alive
 from datetime import *
 from animelist import *
+import pytz
+import discord
 
-command = {
-    '.date': 'Show current date.',
-    '.anime': 'Random an anime for u. :)',
-}
-bot = discord.Client()
+# bot = discord.Client()
+client = commands.Bot(command_prefix=".")
 
 
+@client.event
 async def on_ready():
-    print("Chillin' is ready for his duty.")
+    print("Chillin' bot is ready for her duty!")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for .anime"))
 
 
-@bot.event
-async def on_message(message):
-    send = message.channel.send
-    msg = message.content.lower()
-    if message.author == bot.user:
-        return
+@client.command(aliases=['dat'])
+async def date(ctx):
+    now = datetime.now(pytz.timezone('Asia/Bangkok'))
+    text = str(now.strftime("`%A, %d %b %Y %H:%M %p`"))
+    d = discord.Embed(title=text, color=discord.Colour.random())
+    await ctx.send(embed=d)
 
-    if msg.startswith(".command"):
-        s = ""
-        for i in command:
-            s += i + "  :  " + command[i] + "\n"
-        async with message.channel.typing():
-            await asyncio.sleep(0.25)
-        await send(s)
 
-    if msg in [".dat", ".date"]:  # date function
-        async with message.channel.typing():
-            await asyncio.sleep(0.5)
-        now = datetime.now(pytz.timezone('Asia/Bangkok'))
-        await send(str(now.strftime("```css\n Date : %A, %d %b %Y %H:%M%p```")))
+@client.command(aliases=['ani', 'anim', 'a'])
+async def anime(ctx):
+    i = list(animes.items())
+    anime = choice(i)[0]
 
-    if message.content.startswith(".anime"):  # Random Anime
-        i = list(animes.items())
-        anime = choice(i)[0]
-        e = ["(o´▽`o)", "(¯▿¯)", "(˙꒳​˙)"]
-        a1 = choice(e)
-        e.pop(e.index(a1))
-        a2 = choice(e)
+    embed = discord.Embed(title=f"{anime}", url=f"{link[anime]}",
+                          colour=discord.Colour.random())
 
-        embed = discord.Embed(title=f"{anime}",
-                              url=f"{link[anime]}",
-                              colour=discord.Colour.random())
+    episode = "The movie" if ep[anime] == '1' else ep[anime]+" episodes"
+    ss = season[anime]
 
-        embed.set_author(name=message.author.display_name,
-                         icon_url=message.author.avatar_url)
+    embed.set_author(name=ctx.author.display_name,
+                     icon_url=ctx.author.avatar_url)
 
-        g = genre[anime].replace("[", " ").replace("]", " ").replace("\'", " ")
-        embed.add_field(name="Genre", value=g, inline=False)
+    embed.add_field(name="Length", value=episode, inline=True)
+    embed.add_field(name="Season", value=ss, inline=True)
 
-        episode = "The Movies" if ep[anime] == '1' else  f"Episodes : {ep[anime]}"
+    embed.set_image(url=f"{pic[anime]}")
 
-        embed.add_field(name=episode, value="\u200b", inline=True)
+    embed.set_footer(text="Bot developed by Naxocist")
 
-        embed.set_image(url=f"{pic[anime]}")
+    await ctx.send(embed=embed)
 
-        r = rank[anime]
-        r = "Invalid" if r == "" else r
-        embed.add_field(name=f"Rank : {r}", value="\u200b", inline=True)
-        embed.set_footer(text="Bot developed by Naxocist")
-        await send(embed=embed)
+
+@client.command()
+async def command(ctx):
+    com = {
+        '.date': 'Show current date.',
+        '.anime': 'Random an anime for u. :)',
+    }
+    c = discord.Embed(title="Commands", color=discord.Colour.dark_blue())
+
+    for k, v in com.items():
+        c.add_field(name=k, value=v)
+
+    await ctx.send(embed=c)
 
 
 alive()
-bot.run('ODc3NDI1Mzg0ODY0NTAxNzYw.YRycEQ.qLrJI_seoWMFxZ6dk3O83pCY568')
+client.run('ODc3NDI1Mzg0ODY0NTAxNzYw.YRycEQ.qLrJI_seoWMFxZ6dk3O83pCY568')
