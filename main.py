f5cc95a3774a71d5b219bb1e926d12783d7c8b24
Naxocist <73634957+Naxocist@users.noreply.github.com>
2021-09-05 13:38:53 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,26 +5,43 @@ from datetime import *
 from animelist import *
 import pytz
 import discord
+import asyncio
 
-# bot = discord.Client()
-client = commands.Bot(command_prefix=".")
+# Initialize bot client
+client = commands.Bot(command_prefix=".",
+                      activity=discord.Activity(type=discord.ActivityType.watching, name="for .command"))
 
 
-@client.event
+@client.event  # setup
 async def on_ready():
     print("Chillin' bot is ready for her duty!")
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for .anime"))
 
 
-@client.command(aliases=['dat'])
+@client.command()  # shows command
+async def command(ctx):
+    command = {
+        '.help': 'Display all command',
+        '.date': 'Show current date.',
+        '.anime': 'Random an anime for u. :)',
+
+    }
+    c = discord.Embed(title="Commands", color=discord.Colour.dark_blue())
+
+    for k, v in command.items():
+        c.add_field(name=k, value=v)
+        
+    await ctx.send(embed=c)
+
+
+@client.command(aliases=['dat'])  # date function
 async def date(ctx):
     now = datetime.now(pytz.timezone('Asia/Bangkok'))
-    text = str(now.strftime("`%A, %d %b %Y %H:%M %p`"))
+    text = str(now.strftime("%A, %d %b %Y %H:%M %p"))
     d = discord.Embed(title=text, color=discord.Colour.random())
     await ctx.send(embed=d)
 
 
-@client.command(aliases=['ani', 'anim', 'a'])
+@client.command(aliases=['ani', 'anim', 'a'])  # random anime
 async def anime(ctx):
     i = list(animes.items())
     anime = choice(i)[0]
@@ -35,31 +52,20 @@ async def anime(ctx):
     episode = "The movie" if ep[anime] == '1' else ep[anime]+" episodes"
     ss = season[anime]
 
-    embed.set_author(name=ctx.author.display_name,
-                     icon_url=ctx.author.avatar_url)
+    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
 
     embed.add_field(name="Length", value=episode, inline=True)
     embed.add_field(name="Season", value=ss, inline=True)
 
-    embed.set_image(url=f"{pic[anime]}")
+    embed.set_thumbnail(url=f"{pic[anime]}")
 
     embed.set_footer(text="Bot developed by Naxocist")
 
     await ctx.send(embed=embed)
 
 
-@client.command()
-async def command(ctx):
-    com = {
-        '.date': 'Show current date.',
-        '.anime': 'Random an anime for u. :)',
-    }
-    c = discord.Embed(title="Commands", color=discord.Colour.dark_blue())
-
-    for k, v in com.items():
-        c.add_field(name=k, value=v)
-
-    await ctx.send(embed=c)
+# @client.command()
+# async def grab(ctx):
 
 
 alive()
