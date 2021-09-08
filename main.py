44b658a31e849ea8ddcf98622d8ad967081fa85b
Naxocist <73634957+Naxocist@users.noreply.github.com>
2021-09-08 14:19:49 +0700

    Doujin function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from discord.ext import commands
-from random import *
+from bs4 import BeautifulSoup
 from alive import alive
+from random import *
 from datetime import *
 from animelist import *
 import pytz
 import discord
 import os
+import requests
 import asyncio
+
 
 gif_list = ["https://www.icegif.com/wp-content/uploads/icegif-10.gif",
             "https://i.kym-cdn.com/photos/images/newsfeed/000/543/398/2f3.gif",
@@ -19,10 +22,10 @@ gif_list = ["https://www.icegif.com/wp-content/uploads/icegif-10.gif",
 command_ = {
     '.date | .dat 🕒': 'Show current date and time',
     '.anime | .a    (●≧ω≦)9': 'Somehow pick random anime for fun!',
-    '.animehentai | .ah ⚠': 'You know... It must be in NSFW channel.'
+    '.animehentai | .ah ⚠': 'Delivered random hentai... :mailbox_with_mail:',
+    '.doujin [sauce here]| .d [sauce here] ⚠': 'Exactly, It\'s doujinshi :underage:'
 }
 
-# Initialize bot client
 client = commands.Bot(command_prefix=".",
                       activity=discord.Activity(type=discord.ActivityType.watching, name="for .help"))
 client.remove_command('help')
@@ -35,7 +38,6 @@ async def on_ready():
 
 @client.command(pass_context=True, aliases=["commands", "command"])
 async def help(ctx):
-    author = ctx.message.author
     embed = discord.Embed(title="Commands", color=discord.Colour.random())
 
     for k, v in command_.items():
@@ -90,8 +92,7 @@ async def anime(ctx):
 @commands.is_nsfw()
 async def animehentai(ctx):
     anime = choice(nsfw)
-    embed = discord.Embed(title=f"{anime}", url=f"{link[anime]}",
-                          colour=discord.Colour.random())
+    embed = discord.Embed(title=f"{anime}", url=link[anime], colour=discord.Colour.random())
 
     episode = "The movie" if ep[anime] == '1' else ep[anime] + " episodes"
     ss = "not specified" if episode == "The movie" else season[anime]
@@ -102,7 +103,7 @@ async def animehentai(ctx):
     if len(g_list) > 4:
         g_list.insert(4, "\n")
         g_value = ' '.join(g_list)
-    print(g_value)
+
     embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
     embed.add_field(name="Genre", value=g_value, inline=False)
     embed.add_field(name="Season", value=ss, inline=True)
@@ -113,8 +114,31 @@ async def animehentai(ctx):
     embed.set_thumbnail(url=choice(gif_list))
     embed.set_image(url=image)
     embed.set_footer(text="Dataset based on MyAnimelist.net")
-
+    async with ctx.typing():
+        await asyncio.sleep(0.1)
     await ctx.send(embed=embed)
+
+
+@client.command(aliases=["d", "dou"])
+@commands.is_nsfw()
+async def doujin(ctx, code):
+    url = 'https://nhentai.net/g/' + code
+    print(url)
+    request = requests.get(url).text
+    soup = BeautifulSoup(request, 'html.parser')
+
+    error = soup.find("div", class_="container error")
+    print("Error check:", error)
+    if error:
+        await ctx.send("` Fake Sauce!! `")
+    else:
+        pic = soup.find("img", class_="lazyload")['data-src']
+        name = soup.find("span", class_="pretty").text
+        embed = discord.Embed(title=name, url=url, color=discord.Colour.random())
+        embed.add_field(name=f"Sauce `{code}` delivered!", value="Verified Sauce! Here you go.")
+        embed.set_image(url=pic)
+
+        await ctx.send(embed=embed)
 
 
 @client.event
@@ -122,6 +146,7 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.NSFWChannelRequired):
         warn = "⚠ This is not NSFW channel... ⚠"
         await ctx.send(embed=discord.Embed(title=warn, color=discord.Colour.from_rgb(225, 225, 0)))
+
 
 alive()
 TOKEN = os.environ.get('TOKEN')
